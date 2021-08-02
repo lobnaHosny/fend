2648fb677ace61cf705867657d5ff2ca46d1b53a
Lobna <hcyla1@nottingham.edu.my>
2021-08-02 19:56:36 +0200

    set up express and middleware

--- projects/weather-journal-app/commentsOnlyJS/server.js
+++ projects/weather-journal-app/commentsOnlyJS/server.js
@@ -1,13 +1,21 @@
 // Setup empty JS object to act as endpoint for all routes
 // Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Dependencies */
 /* Middleware*/
 
 //Here we are configuring express to use body-parser as middle-ware.
 // Cors for cross origin allowance
+const bodyParser = require('bpdy-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors);
 
 // Initialize the main project folder
 
